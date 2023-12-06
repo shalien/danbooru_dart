60f009cf40ce63c73e06be632121a4eec10a7eec
Ophélien DUPARC <ophelien.duparc@projetretro.io>
2023-12-06 18:40:07 +0100

    new tools

---
+++ test/extract/extract.dart
@@ -0,0 +1,27 @@
+void main() async {
+
+  const String base = 'https://danbooru.donmai.us';
+
+  const endpoints = List.unmodifiable([
+    'posts',
+    'autocomplete',
+    '/',
+    'admin/dashboard',
+    'moderator/dashboard',
+      'explore/posts/popular',
+    'explore/posts/curated',
+    'explore/posts/viewed',
+    'explore/posts/searches',
+    'explore/posts/missed_searches',
+    'artists',
+    'bans',
+    'comments',
+    'counts',
+    'dmails',
+    'favorites',
+    'forum_posts',
+  ]);
+
+
+
+}
---
+++ tool/endpoint_to_csv.dart
@@ -0,0 +1,18 @@
+import 'dart:io';
+
+void main() {
+
+  File endpointsFile = File('tool/_endpoint');
+
+  List<String> endpoints = endpointsFile.readAsLinesSync();
+
+  File csvFile = File('tool/_endpoint.csv');
+
+  /// Split  on space , remove empty space koin with ;
+  List<String> csvLines = endpoints.map((e) => e.split(' ').where((element) => element.isNotEmpty).join(';')).toList();
+
+  csvFile.writeAsStringSync(csvLines.join(Platform.lineTerminator));
+
+exit(0);
+
+}
